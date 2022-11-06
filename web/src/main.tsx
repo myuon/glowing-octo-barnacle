8e8165c5d705ecceac5287921b1cf903fce78814
myuon <ioi.joi.koi.loi@gmail.com>
2022-11-06 21:13:06 +0900

    chore: configure pwa

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,9 +4,19 @@ import App from "./App";
 import "ress";
 import "./index.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { registerSW } from "virtual:pwa-register";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+registerSW({
+  onNeedRefresh() {
+    return;
+  },
+  onOfflineReady() {
+    return;
+  },
+});
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
+    }),
+    VitePWA({
+      includeAssets: ["favicon.svg", "favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "kakeibo",
+        short_name: "kakeibo",
+        description: "",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        // cf: https://github.com/NekR/offline-plugin/issues/412
+        navigateFallbackDenylist: [/\/__\/auth/],
+      },
     }),
   ],
   build: {
