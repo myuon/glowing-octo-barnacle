081aaab498f5e17be31a0f4598abda5c946c5a29
myuon <ioi.joi.koi.loi@gmail.com>
2022-11-20 19:50:42 +0900

    fix: catch for onAuthStateChanged

--- web/src/components/auth.tsx
+++ web/src/components/auth.tsx
@@ -1,5 +1,17 @@
 import { auth } from "../api/firebase";
 
 export const getAuthToken = async () => {
-  return await auth.currentUser?.getIdToken();
+  if (auth.currentUser) {
+    return await auth.currentUser?.getIdToken();
+  } else {
+    return new Promise((resolve, reject) => {
+      const unsubscribe = auth.onAuthStateChanged(async (user) => {
+        unsubscribe();
+        if (user) {
+          resolve(await user.getIdToken());
+        }
+        reject();
+      });
+    });
+  }
 };
